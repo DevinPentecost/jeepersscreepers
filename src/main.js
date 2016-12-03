e4fdf25faf2b21dccca0af1461c8636b0b9e8e34
dpentecost <devinpentecost@gmail.com>
2016-12-03 14:53:00 -0800

    Move to src

---
+++ src/main.js
@@ -0,0 +1,52 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+var roleSpawner = require('role.spawner');
+var roleRepairer = require('role.repairer');
+
+module.exports.loop = function () {
+    
+    for(var i in Memory.creeps) {
+        if(!Game.creeps[i]) {
+            delete Memory.creeps[i];
+        }
+    }
+
+    var tower = Game.getObjectById('bd05d106b72310b59b08c205');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+        if(creep.memory.role == 'repairer') {
+            roleRepairer.run(creep);
+        }
+    }
+    
+    for(var name in Game.spawns){
+        var spawn = Game.spawns[name];
+        if(spawn.memory.role == 'spawner'){
+            roleSpawner.run(spawn);
+        }
+    }
+}
---
+++ src/role.spawner.js
@@ -0,0 +1,75 @@
+//What is the minimum creeps per resource in this room?
+var MINIMUM_HARVESTERS_PER_RESOURCE = 2;
+var MINIMUM_UPGRADERS_PER_ROOM = 1;
+var MINIMUM_BUILDERS_PER_ROOM = 2;
+var MINIMUM_REPAIRERS_PER_ROOM = 2;
+
+var roleSpawner = {
+
+    /** @param {Spawner} spawner **/
+    run: function(spawner) {
+        //Figure out how many creeps are harvesters
+        var totalHarvesters = 0;
+        var totalUpgraders = 0;
+        var totalBuilders = 0;
+        var totalRepairers = 0;
+        var roomCreeps = spawner.room.find(FIND_MY_CREEPS)
+        for(var creepIndex in roomCreeps) {
+            
+            var creep = roomCreeps[creepIndex]
+            
+            if(creep.memory.role == 'harvester') {
+                totalHarvesters = totalHarvesters + 1;
+            }
+            if(creep.memory.role == 'upgrader') {
+                totalUpgraders = totalUpgraders + 1;
+            }
+            if(creep.memory.role == 'builder') {
+                totalBuilders = totalBuilders + 1;
+            }
+            if(creep.memory.role == 'repairer'){
+                totalRepairers = totalRepairers + 1;
+            }
+        }
+        
+        //How many resources are there?
+        var sources = spawner.room.find(FIND_SOURCES);
+        var totalSources = sources.length;
+        
+        //Do we need more?
+        console.log(totalHarvesters + ' ' + totalUpgraders + ' ' + totalBuilders + ' ' + totalRepairers)
+        var requiredHarvesters = MINIMUM_HARVESTERS_PER_RESOURCE * totalSources;
+        var enoughHarvesters = totalHarvesters >= requiredHarvesters
+        if(!enoughHarvesters){
+            //We need to create one
+            console.log("Need a Harvester");
+            newCreep = spawner.createCreep([MOVE, WORK, WORK, CARRY], {role: "harvester"});
+        }else{
+            //We can make other stuff
+            //Enough upgraders?
+            if(MINIMUM_UPGRADERS_PER_ROOM > totalUpgraders){
+                //Make one
+                console.log("Need a Upgrader");
+                newCreep = spawner.createCreep([MOVE, WORK, CARRY], {role: "upgrader"});
+            }
+            
+            //Enough builders?
+            if(MINIMUM_BUILDERS_PER_ROOM > totalBuilders){
+                //Make one
+                console.log("Need a Builder");
+                newCreep = spawner.createCreep([MOVE, WORK, CARRY, CARRY], {role: "builder"});
+            }
+            
+            //Enough repairers?
+            if(MINIMUM_REPAIRERS_PER_ROOM > totalRepairers){
+                //Make one
+                console.log("Need a Repairer");
+                newCreep = spawner.createCreep([MOVE, WORK, CARRY, CARRY], {role: "repairer"});
+            }
+        }
+        
+        
+    }
+};
+
+module.exports = roleSpawner;
